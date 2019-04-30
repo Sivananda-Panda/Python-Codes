4ce2c1b3d0f2c23c24bcf8b302b4995cb22e3ba6
Sivananda Panda <sivananda.panda@gmail.com>
2019-05-01 00:40:10 +0200

    Test-1
    Lets see

--- Next largest element.py
+++ Next largest element.py
@@ -69,4 +69,82 @@ for i in range(t):
     for i in res:
         print(i, end=' ')
     print()
-    
+#from office
+#code
+t=int(input())
+for i in range(t):
+    def gl():
+        length=int(input())
+        li=list(map(int, input().split()))
+        li2=[]
+        res=[]
+        '''for i in range(length):
+            res.append(0)'''
+        
+        for i in range(len(li)):
+            flag=False
+            while (len(li2)!=0 and li[i]>li2[-1]):
+                res.insert(li.index(li2[-1]), li[i])
+                li2.pop(len(li2)-1)
+                flag=True
+            if flag:
+                li2.append(li[i])
+            
+            if (len(li2)==0):
+                li2.append(li[i])
+            elif li[i]<li2[-1]:
+                li2.append(li[i])
+        
+        c=len(li2)
+        while (c!=0):
+            res.insert(li.index(li2[-1]), -1)
+            c=c-1
+        return res
+    res1=gl()
+    for j in res1:
+        print(j, end=' ')
+    print()
+#final
+t=int(input())
+for i in range(t):
+    length=int(input())
+    li=list(map(int, input().split()))
+    sta=[]
+    res=[]
+    for i in range(len(li)):
+        flag=False
+        while(len(sta)!=0):
+            if li[i]>sta[-1]:
+                
+                res.insert(li.index(sta[-1]), li[i])
+                sta.pop(len(sta)-1)
+                
+                flag=True
+            elif li[i]==sta[-1]:
+                res.insert(li.index(sta[-1]), -1)
+                sta.pop(len(sta)-1)
+                
+                flag=True
+            elif li[i]<sta[-1]:
+                sta.append(li[i])
+                flag=False
+                break
+        if flag:
+            sta.append(li[i])
+        if len(sta)==0:
+            sta.append(li[i])
+        
+            
+        c=len(sta)
+        if c==len(li):
+            while c:
+                res.insert(li.index(sta[-1]), -1)
+                c=c-1
+        elif i==len(li)-1 and c!=0:
+            while len(sta)!=0:
+                res.insert(li.index(sta[-1]), -1)
+                sta.pop(len(sta)-1)
+            
+    for i in res:
+        print(i, end=' ')
+    print()
